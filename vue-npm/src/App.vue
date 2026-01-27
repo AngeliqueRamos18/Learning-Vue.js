0c96fcf6cef58a18424ada312900bdbb515cad46
ChiAngel18 <angeliqueramos.dev@gmail.com>
2026-01-27 20:27:54 +0800

    Added Card

--- vue-npm/src/App.vue
+++ vue-npm/src/App.vue
@@ -1,4 +1,15 @@
-<template>
-  <h1>Vue Jobs</h1>
-</template>
+<script setup>
+// some of the references files from created project were used (_theme_files)
+// In this part we are calling the Navbar.vue file to import to our main App 
+  import Navbar from '@/components/Navbar.vue'
+  import Hero from '@/components/Hero.vue'
+  import HomeCards from '@/components/HomeCards.vue'
 
+</script>
+
+<template>
+  <Navbar />
+  <!-- Refer to the file src/components/Hero.vue to check the default display output if the certain parameters were not called-->
+  <Hero subtitle="Test Subtitle"/>
+  <HomeCards />
+</template>
